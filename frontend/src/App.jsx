3ae6835765180c628ad4b0d5175a0032aede825f
MrSilent2001 <bandarigodadm.21@uom.lk>
2025-04-22 14:22:57 +0530

    basic frontend file structure created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,19 @@
-import { useState } from 'react'
 import './App.css'
+import {Route, Routes} from "react-router-dom";
+import Dashboard from "./pages/dashboard.tsx";
+import Welcome from "./pages/welcome.js";
+import Login from "./pages/login.js";
+import CreatePatients from "./pages/createPatients.js";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>Your implemenation goes here</div>
-    </>
+    <Routes>
+        <Route path="/" element={<Welcome/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/home" element={<Dashboard/>} />
+        <Route path="/add-new-patient" element={<CreatePatients/>} />
+    </Routes>
   )
 }
 
---
+++ frontend/src/pages/welcome.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Welcome: React.FC = () => {
+    return(
+        <>
+            <h1>Welcome to MedCare!</h1>
+        </>
+    )
+}
+
+export default Welcome;
