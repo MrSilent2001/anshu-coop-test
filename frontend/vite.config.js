600b3f59c4f05ede468bced718929cf00dd7c953
MrSilent2001 <bandarigodadm.21@uom.lk>
2025-04-24 12:41:25 +0530

    frontend bugs fixed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,22 +1,12 @@
-import { fileURLToPath } from 'url';
-import path from 'path';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// Polyfill __dirname
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    port: 3100,
-  },
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-});
+      '@': path.resolve(__dirname, './src')
+    }
+  }
+})
